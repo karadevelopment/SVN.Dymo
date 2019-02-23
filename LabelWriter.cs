91a926f74ffeeef629ba06e8bf3eb66ac284c083
Muhammed Kara <47905966+svndevelopment@users.noreply.github.com>
2019-02-23 01:38:58 +0100

    1.0.1.0

---
+++ LabelWriter.cs
@@ -0,0 +1,45 @@
+﻿using DYMO.Label.Framework;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SVN.Dymo
+{
+    public class LabelWriter
+    {
+        private const string WRITER_450 = "DYMO LabelWriter 450";
+        private string TemplateUri { get; }
+
+        public LabelWriter(string templateUri)
+        {
+            this.TemplateUri = templateUri;
+        }
+
+        public IEnumerable<string> GetKeys()
+        {
+            using (var stream = new FileStream(this.TemplateUri, FileMode.Open))
+            {
+                var label = Label.Open(stream);
+
+                foreach (var objectName in label.ObjectNames)
+                {
+                    yield return objectName;
+                }
+            }
+        }
+
+        public void Print(params (string key, object value)[] fields)
+        {
+            using (var stream = new FileStream(this.TemplateUri, FileMode.Open))
+            {
+                var label = Label.Open(stream);
+
+                foreach (var (key, value) in fields)
+                {
+                    label.SetObjectText(key, value.ToString());
+                }
+
+                label.Print(LabelWriter.WRITER_450);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SVN.Dymo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SVN Development")]
+[assembly: AssemblyProduct("SVN.Dymo")]
+[assembly: AssemblyCopyright("Copyright © Muhammed Kara 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4227e5a9-9ec8-4f94-b3fc-f0c0e28081e5")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
